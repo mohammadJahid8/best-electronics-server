004a24d271a548e0524f4593f1aac19ee8e990dc
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-04 18:01:34 +0600

    loaded data from database by get method

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
 require('dotenv').config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
 
 
 
@@ -14,10 +21,16 @@ const run = async () => {
 
     try {
         await client.connect();
+        const itemsCollection = client.db("inventory").collection("items");
 
-
-        app.get('/', (req, res) => {
-            res.send('welcome to the server')
+        //get all items from database
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+            // console.log(result);
+            // console.log('hello');
         })
 
     }
