a06e383e2319516e2072ba145cfc1ea3102a7cb1
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-30 12:24:41 +0600

    checking

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ const verifyToken = (req, res, next) => {
 }
 
 
+
+//connecting to mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxcla.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
